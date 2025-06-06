e18b934431fe759a3a3b2935e2df069065c9b855
koha0726 <kohayakawa.0726@gmail.com>
2025-06-06 15:56:56 +0900

    add logo

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function home() {
+  document.getElementById('start-screen').style.display = 'block';
+  document.getElementById('quiz-screen').style.display = 'none';
+  document.getElementById('result-screen').style.display = 'none';
+}
+
 function startQuiz() {
   alert("ここでクイズを開始します（仮）");
   document.getElementById('start-screen').style.display = 'none';
