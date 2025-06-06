2237d9e094bc69764b1d8f60fa7666ca13cad789
koha0726 <kohayakawa.0726@gmail.com>
2025-06-06 10:06:31 +0900

    scroll test

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function startQuiz() {
   alert("ここでクイズを開始します（仮）");
+  document.getElementById('start-screen').style.display = 'none';
+  document.getElementById('quiz-screen').style.display = 'block';
+  
+}
+
+function showResult() {
+  document.getElementById('result-screen').style.display = 'block';
 }
