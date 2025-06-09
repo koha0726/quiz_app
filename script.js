12cb723bc2c37cb30a71507dec871c5c041ea265
koha0726 <kohayakawa.0726@gmail.com>
2025-06-09 16:22:58 +0900

    answer add

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function home() {
 }
 
 let quizData = []; // クイズ内容の保持
+let currentQuestionIndex = 0; // インデックス番号の保持
 
 // 問題画面
 function startQuiz() {
@@ -20,7 +21,7 @@ function startQuiz() {
       console.log(data); 
 
       // 一時表示
-      showQuestion(0);
+      showQuestion(1);
     })
     .catch(err => {
       console.error('JSONの読み込みに失敗:', err);
@@ -28,7 +29,9 @@ function startQuiz() {
 
 }
 
+// 問題文・選択肢の表示
 function showQuestion(index) {
+  currentQuestionIndex = index; // 現在の問題番号
   const question = quizData[index];
 
   if (!question) {
@@ -54,16 +57,47 @@ function showQuestion(index) {
 
     input.type = inputType;
     input.name = 'choice';
-    input.value = choice; // 選択肢のテキストをvalue属性にセットする
+    input.value = choice; // 選択肢のテキストをvalue属性にセット
 
     // ラベルに選択ボタンとテキストを追加
     label.appendChild(input);
     label.appendChild(document.createTextNode(choice));
 
     const br = document.createElement('br');
+    // 子要素として追加
     choicesContainer.appendChild(label);
     choicesContainer.appendChild(br);
   });
+}
+
+// 正誤判定
+function checkAnswer(index) {
+  const question = quizData[index];
+  const inputs = document.querySelectorAll('input[name="choice"]'); // 現在のすべての選択肢の要素を取得
+  const selected = [];
+
+  // 選択されているものだけをselectedに追加
+  inputs.forEach(input => {
+    if (input.checked) {
+      selected.push(input.value);
+    }
+  });
+
+  // 正解の数の変化の対応
+  const correct = Array.isArray(question.answer) ? question.answer : [question.answer];
+
+  // 数と選択の一致の確認
+  const isCorrect = selected.length === correct.length && selected.every(choice => correct.includes(choice));
+
+  const answerDiv = document.getElementById('answer');
+  if (isCorrect) {
+    answerDiv.innerText = "正解！";
+    answerDiv.style.color = "green";
+  } else {
+    answerDiv.innerText = "不正解";
+    answerDiv.style.color = "red";
+  }
+
 }
 
 // 解説
